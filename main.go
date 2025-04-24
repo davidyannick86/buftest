eac64efecfed876b8ee2b8c1b7b9feabd3ee8500
DavidYannick <57354908+davidyannick86@users.noreply.github.com>
2025-04-24 07:41:23 +0200

    refactor: enhance server startup with graceful shutdown and error handling

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"os/signal"
+	"syscall"
 
 	"net"
 	"net/http"
@@ -13,6 +16,7 @@ import (
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"golang.org/x/sync/errgroup"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -40,32 +44,85 @@ func (s *server) SayHello(ctx context.Context, req *proto_hello.SayHelloRequest)
 	return response, nil
 }
 
+var interruptSignals = []os.Signal{
+	os.Interrupt,
+	syscall.SIGTERM,
+	syscall.SIGINT,
+}
+
 func main() {
 
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
-	go func() {
-		grpcMux := runtime.NewServeMux()
-		err := proto_hello.RegisterHelloServiceHandlerServer(context.Background(), grpcMux, &server{})
+	ctx, cancel := signal.NotifyContext(
+		context.Background(),
+		interruptSignals...,
+	)
+
+	defer cancel()
+
+	log.Info().Msg("Starting server...")
+
+	wg, ctx := errgroup.WithContext(ctx)
+
+	runGrpcServer(ctx, wg)
+	runHTTOGateway(ctx, wg)
+
+	err := wg.Wait()
+	if err != nil {
+		log.Error().Err(err).Msg("Error in server")
+	} else {
+		log.Info().Msg("Server stopped gracefully")
+	}
+
+}
+
+func runHTTOGateway(
+	ctx context.Context,
+	wg *errgroup.Group,
+) {
+	grpcMux := runtime.NewServeMux()
+	err := proto_hello.RegisterHelloServiceHandlerServer(context.Background(), grpcMux, &server{})
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to register handler")
+	}
+	mux := http.NewServeMux()
+	mux.Handle("/", grpcMux)
+
+	httpServer := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: mux,
+	}
+
+	wg.Go(func() error {
+		log.Info().Msgf("HTTP Gateway server listening at %v", httpServer.Addr)
+		err := httpServer.ListenAndServe()
 		if err != nil {
-			log.Fatal().Err(err).Msg("failed to register handler")
+			if errors.Is(err, http.ErrServerClosed) {
+				return nil
+			}
+			log.Error().Err(err).Msg("failed to serve")
+			return err
 		}
-		mux := http.NewServeMux()
-		mux.Handle("/", grpcMux)
+		return nil
+	})
 
-		listener, err := net.Listen("tcp", ":8080")
-		if err != nil {
-			log.Fatal().Err(err).Msg("failed to listen")
+	wg.Go(func() error {
+		<-ctx.Done()
+		log.Info().Msg("Stopping HTTP Gateway server...")
+		if err := httpServer.Shutdown(ctx); err != nil {
+			log.Error().Err(err).Msg("failed to shutdown HTTP server")
+			return err
 		}
+		log.Info().Msg("HTTP Gateway server stopped")
+		return nil
+	})
+}
 
-		log.Info().Msgf("Gateway server listening at %v", listener.Addr())
-
-		if err := http.Serve(listener, mux); err != nil {
-			log.Fatal().Err(err).Msg("failed to serve")
-		}
-
-	}()
-
+func runGrpcServer(
+	ctx context.Context,
+	wg *errgroup.Group,
+) {
 	grpcServer := grpc.NewServer()
 	proto_hello.RegisterHelloServiceServer(grpcServer, &server{})
 
@@ -74,12 +131,27 @@ func main() {
 		log.Fatal().Err(err).Msg("failed to listen")
 	}
 
-	log.Info().Msgf("GRPC server listening at %v", listener.Addr())
-
 	reflection.Register(grpcServer)
 
-	if err := grpcServer.Serve(listener); err != nil {
-		log.Fatal().Err(err).Msg("failed to serve")
-	}
+	wg.Go(func() error {
+		log.Info().Msgf("GRPC server listening at %v", listener.Addr())
+
+		if err := grpcServer.Serve(listener); err != nil {
+			if errors.Is(err, grpc.ErrServerStopped) {
+				return nil
+			}
+			log.Error().Err(err).Msg("failed to serve")
+			return err
+		}
+		return nil
+	})
+
+	wg.Go(func() error {
+		<-ctx.Done()
+		log.Info().Msg("Stopping GRPC server...")
+		grpcServer.GracefulStop()
+		log.Info().Msg("GRPC server stopped")
+		return nil
+	})
 
 }
